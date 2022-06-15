bf99bf371288c570a49dd747459a315f9d690ffa
kkennyllow <99718568+kkennyllow@users.noreply.github.com>
2022-06-15 18:29:37 +0800

    Implement deletion of bookmark

--- Telegram_Bot.py
+++ Telegram_Bot.py
@@ -13,34 +13,34 @@ API_KEY = os.environ['API_KEY']
 bot = telebot.TeleBot(API_KEY)
 bot.delete_my_commands(scope=None, language_code=None)
 
-
 #########################
 ### CALLBACK CHECKERS ###
 #########################
 
-
 def isPackage(call):
-  if "java." in call.data or "javax." in call.data:
-    return True
-  return False
+  return "java." in call.data or "javax." in call.data
 
 def isClass(call):
   string = call.data
-  if string[0].isupper() and not isBookmark(call):
-    return True
-  return False
+  return string[0].isupper() and not isBookmark(call)
+
+def isMethod(call):
+  return not isPackage(call) and not isClass(call) and not isBookmark(call)
 
 def isBookmark(call):
-  if call.data == "Bookmark" :
-    return True
-  return False
-  
-def isBookmarkedMethod(call):
-  return not isPackage(call) and not isClass(call) and not isBookmark(call) and "bookmark " in call.data
-  
-def isMethod(call):
-  return not isPackage(call) and not isClass(call) and not isBookmark(call) and "bookmark " not in call.data
+  return "bookmark" in call.data
 
+def isAddBookmark(call):
+  return call.data == "add bookmark"
+
+def isDisplayBookmarks(call):
+  return isBookmark(call) and "display bookmark " in call.data
+
+def isRemoveBookmark(call):
+  return isBookmark(call) and "removing bookmark " in call.data
+
+def isReturnToBookmarks(call):
+  return isBookmark(call) and "display all bookmarks " in call.data
 
   
 #################
@@ -96,7 +96,7 @@ def keyboardForMethods(post):
       
 def keyboardForAddingBookmark():
   keyboard_inline = InlineKeyboardMarkup()
-  keyboard_inline = keyboard_inline.add(InlineKeyboardButton(text = "Bookmark",callback_data = "Bookmark"))
+  keyboard_inline = keyboard_inline.add(InlineKeyboardButton(text = "Bookmark",callback_data = "add bookmark"))
   return keyboard_inline
 
 def keyboardForSearchingBookmarks(id):
@@ -104,12 +104,15 @@ def keyboardForSearchingBookmarks(id):
   users = db["Bookmark"]
   bookmarked_methods = users.find_one({'chat_id' : id})["Bookmarked_methods"]
   for methods in bookmarked_methods:
-    button = InlineKeyboardButton(text = methods['method_name'],callback_data = "bookmark " + methods['method_name'])
+    button = InlineKeyboardButton(text = methods['method_name'],callback_data = "display bookmark " + methods['method_name'])
     keyboard_inline.add(button)
   return keyboard_inline       
         
-    
-
+def keyboardForRemovingBookmarks(bookmarked_method):  
+  keyboard_inline = InlineKeyboardMarkup()
+  keyboard_inline.add(InlineKeyboardButton(text = "Back to Bookmarks",callback_data = "display all bookmarks " + bookmarked_method))
+  keyboard_inline.add(InlineKeyboardButton(text = "Remove Bookmark",callback_data = "removing bookmark " + bookmarked_method))
+  return keyboard_inline   
 
   
 #########################
@@ -123,19 +126,16 @@ def whichPackage(message):
 @bot.callback_query_handler(func= isPackage)
 def whichClass(call):
   classname = call.data
-
   bot.delete_message (call.message.chat.id, call.message.id)
-  bot.send_message(call.message.chat.id, "Which class?", reply_markup = keyboardForClasses())
+  bot.send_message(call.message.chat.id, "Which class?", reply_markup = keyboardForClasses(classname))
   
   @bot.callback_query_handler(func= isClass)
   def whichMethod(call):
     posts = db[classname]
     post = posts.find_one({"classname" : call.data})
-
     bot.delete_message (call.message.chat.id, call.message.id)
     bot.send_message(call.message.chat.id, "Which method?", reply_markup = keyboardForMethods(post))
     
-
     @bot.callback_query_handler(func= isMethod)
     def methodDescription(call):
       methods = post["classmethods"]
@@ -145,9 +145,9 @@ def whichClass(call):
           type = method["method_modifier"]
           description = method["method_description"].replace("\n", "")
           msg = "Method Name: " + name + "\n" + "\n" + "Method Modifier: " + type + "\n" + "\n" + "Method Description: " + description
-          bot.send_message(call.message.chat.id, msg,reply_markup = keyboardForAddingBookmark())
+          bot.send_message(call.message.chat.id, msg, reply_markup = keyboardForAddingBookmark())
   
-      @bot.callback_query_handler(func = isBookmark)
+      @bot.callback_query_handler(func = isAddBookmark)
       def addBookmark(call):
         users = db["Bookmark"]
         if users.find_one({'chat_id' : call.message.chat.id}) != None:
@@ -161,32 +161,48 @@ def whichClass(call):
         bot.send_message(call.message.chat.id,text)
 
 
-##########################
-### QUERYING BOOKMARKS ###
-##########################
+#################
+### BOOKMARKS ###
+#################
 
 @bot.message_handler(commands = ['bookmarks'])
 def bookmarks(message):
   users = db["Bookmark"]
-  if users.find_one({'chat_id' : message.chat.id}) != None:
-      bot.send_message(message.chat.id,"Here are your bookmarks",reply_markup = keyboardForLookingUpBookmarks(message.chat.id)) 
+  if users.find_one({'chat_id' : message.chat.id}) != None and users.find_one({'chat_id' : message.chat.id})["Bookmarked_methods"] != []:
+      bot.send_message(message.chat.id,"Here are your bookmarks",reply_markup = keyboardForSearchingBookmarks(message.chat.id)) 
   else:
     bot.send_message(message.chat.id,"You have no bookmarks")
       
-@bot.callback_query_handler(func=isBookmarkedMethod)
+@bot.callback_query_handler(func=isDisplayBookmarks)
 def displayBookmarkedMethod(call):
   users = db["Bookmark"]
   id = call.message.chat.id
   bookmarked_methods = users.find_one({'chat_id' : id})["Bookmarked_methods"]
   for i in bookmarked_methods:
-    if i['method_name'] == call.data.replace("bookmark ", ""):
+    if i['method_name'] == call.data.replace("display bookmark ", ""):
       name = i["method_name"]
       type = i["method_modifier"]
       description = i["method_description"].replace("\n", "")
       msg = "Method Name: " + name + "\n" + "\n" + "Method Modifier: " + type + "\n" + "\n" + "Method Description: " + description
-      bot.send_message(call.message.chat.id, msg)
+      bot.delete_message (call.message.chat.id, call.message.id)
+      bot.send_message(call.message.chat.id, msg, reply_markup = keyboardForRemovingBookmarks(name))
       
-
-
+@bot.callback_query_handler(func=isRemoveBookmark)
+def removeBookmarkedMethod(call):
+  users = db["Bookmark"]
+  id = call.message.chat.id
+  bookmarked_methods = users.find_one({'chat_id' : id})["Bookmarked_methods"]
+  for i in bookmarked_methods:
+    if i['method_name'] == call.data.replace("removing bookmark ", ""):
+      name = i["method_name"]
+      bookmarked_methods.remove(i)
+      users.update_one({'chat_id': id},{'$set': {"Bookmarked_methods": bookmarked_methods}}, upsert=True)
+      bot.delete_message (call.message.chat.id, call.message.id)
+      bot.send_message(call.message.chat.id, "Bookmarked method " + name + " removed")
+  
+@bot.callback_query_handler(func=isReturnToBookmarks)
+def returnToBookmarks(call):
+  bot.delete_message (call.message.chat.id, call.message.id)
+  return bookmarks(call.message)
 
 bot.polling()
