463015ae8fe2e3ad5afd100f2cb9dbc47c054762
NWH21 <99738981+NWH21@users.noreply.github.com>
2022-07-14 14:34:20 +0800

    Debugging NoneType Error

--- Telegram_Bot.py
+++ Telegram_Bot.py
@@ -122,21 +122,22 @@ def keyboardForMethods(post):
   keyboard_inline = InlineKeyboardMarkup()
   list_id = []
   list = []
+  print(post)
   for methods in post["classmethods"]:
     methodname = methods["method_name"]
     methodname = re.sub("\([^()]*\)", "", methodname)
     list_id.append(methods["ID"]) 
     list.append(methodname)
-  if len(list) == 0:
-    keyboard_inline = keyboard_inline.add(InlineKeyboardButton(text = "No methods", callback_data =None))
-  else:
-    for i in range(len(list)):
-      if i % 2 == 0 and i != len(list) - 1: 
-        keyboard_inline = keyboard_inline.add(InlineKeyboardButton(text = list[i], callback_data = list_id[i]),InlineKeyboardButton(text = list[i+1], callback_data = list_id[i+1]))
-      elif i % 2 == 0 and i == len(list) - 1:
-        keyboard_inline = keyboard_inline.add(InlineKeyboardButton(text = list[i], callback_data = list_id[i]))
-      else:
-        continue
+##  if len(list) == 0:
+##    keyboard_inline = keyboard_inline.add(InlineKeyboardButton(text = "No methods", callback_data =None))
+  
+  for i in range(len(list)):
+    if i % 2 == 0 and i != len(list) - 1: 
+      keyboard_inline = keyboard_inline.add(InlineKeyboardButton(text = list[i], callback_data = list_id[i]),InlineKeyboardButton(text = list[i+1], callback_data = list_id[i+1]))
+    elif i % 2 == 0 and i == len(list) - 1:
+      keyboard_inline = keyboard_inline.add(InlineKeyboardButton(text = list[i], callback_data = list_id[i]))
+    else:
+      continue
   return keyboard_inline 
       
 def keyboardForAddingBookmark():
@@ -242,47 +243,52 @@ def whichPackage(message):
 
 @bot.callback_query_handler(func= isPackage)
 def whichClass(call):
-  classname = call.data
+  package_name = call.data
   bot.delete_message (call.message.chat.id, call.message.id)
-  bot.send_message(call.message.chat.id, "Which class?", reply_markup = keyboardForClasses(classname))
+  bot.send_message(call.message.chat.id, "Which class?", reply_markup = keyboardForClasses(package_name))
   
   @bot.callback_query_handler(func= isClass)
   def whichMethod(call):
-    try:
-      posts = db[classname]
-      post = posts.find_one({"classname" : call.data})
-      bot.delete_message (call.message.chat.id, call.message.id)
-      bot.send_message(call.message.chat.id, "Which method?", reply_markup = keyboardForMethods(post))
+##    try:
+    print(package_name)
+    posts = db[package_name]
+    post = posts.find_one({"classname" : call.data})
+    bot.delete_message (call.message.chat.id, call.message.id)
+    bot.send_message(call.message.chat.id, "Which method?", reply_markup = keyboardForMethods(post))
     
-      @bot.callback_query_handler(func= isMethod)
-      def methodDescription(call):
-        methods = post["classmethods"]
-        for method in methods:
-          if method["ID"] == call.data:
-            name = method["method_name"]
-            type = method["method_modifier"]
-            description = method["method_description"].replace("\n", "")
-            msg = "Method Name: " + name + "\n" + "\n" + "Method Modifier: " + type + "\n" + "\n" + "Method Description: " + description
-            bot.send_message(call.message.chat.id, msg, reply_markup = keyboardForAddingBookmark())
+    @bot.callback_query_handler(func= isMethod)
+    def methodDescription(call):
+      methods = post["classmethods"]
+      for method in methods:
+        if method["ID"] == call.data:
+          name = method["method_name"]
+          type = method["method_modifier"]
+          description = method["method_description"].replace("\n", "")
+          msg = "Method Name: " + name + "\n" + "\n" + "Method Modifier: " + type + "\n" + "\n" + "Method Description: " + description
+          bot.send_message(call.message.chat.id, msg, reply_markup = keyboardForAddingBookmark())
   
-        @bot.callback_query_handler(func = isAddBookmark)
-        def addBookmark(call):
-          users = db["Bookmark"]
-          if users.find_one({'chat_id' : call.message.chat.id}) != None:
-            existing = users.find_one({'chat_id' : call.message.chat.id})
-            existing = existing["Bookmarked_methods"] 
-          else:
-            existing = []  
+      @bot.callback_query_handler(func = isAddBookmark)
+      def addBookmark(call):
+        users = db["Bookmark"]
+        if users.find_one({'chat_id' : call.message.chat.id}) != None:
+          existing = users.find_one({'chat_id' : call.message.chat.id})
+          existing = existing["Bookmarked_methods"] 
+        else:
+          existing = []  
           existing.append({"method_name":name,"method_modifier": type, "method_description": description})
           users.update_one({'chat_id': call.message.chat.id},{'$set': {"Bookmarked_methods":existing}}, upsert=True)
           text = "Bookmark added!"
           bot.send_message(call.message.chat.id,text)
 
-    except TypeError as e:
-      print(e)
-      text = "There are no methods in this class!"
-      bot.send_message(call.message.chat.id,text)
-      return text
+     
+##
+##    except TypeError as e:
+##      print(e)
+##      text = "There are no methods in this class!"
+##      bot.send_message(call.message.chat.id,text)
+##      return text
+
+      
 
 #################
 ### BOOKMARKS ###
